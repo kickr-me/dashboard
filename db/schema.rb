62ebaa459420a9338c819a438a9e7578943c9293
Robert Müller 🚀 <robert.mueller@hetzner-cloud.de>
2022-06-17 21:14:16 +0200

    upgrade to rails 7
    Signed-off-by: Robert Müller 🚀 <robert.mueller@hetzner-cloud.de>

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_17_190447) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_06_17_191145) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.integer "record_id", null: false
     t.integer "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -28,9 +27,9 @@ ActiveRecord::Schema.define(version: 2022_06_17_190447) do
     t.string "content_type"
     t.text "metadata"
     t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
+    t.integer "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", precision: nil, null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -44,15 +43,15 @@ ActiveRecord::Schema.define(version: 2022_06_17_190447) do
     t.integer "badge_id"
     t.integer "sash_id"
     t.boolean "notified_user", default: false
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
     t.index ["badge_id", "sash_id"], name: "index_badges_sashes_on_badge_id_and_sash_id"
     t.index ["badge_id"], name: "index_badges_sashes_on_badge_id"
     t.index ["sash_id"], name: "index_badges_sashes_on_sash_id"
   end
 
   create_table "match_teams", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "match_teams_players", id: false, force: :cascade do |t|
@@ -61,8 +60,8 @@ ActiveRecord::Schema.define(version: 2022_06_17_190447) do
   end
 
   create_table "matches", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "team_a_id"
     t.integer "team_b_id"
     t.integer "team_a_front_id"
@@ -81,8 +80,8 @@ ActiveRecord::Schema.define(version: 2022_06_17_190447) do
     t.integer "target_id"
     t.text "target_data"
     t.boolean "processed", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "merit_activity_logs", force: :cascade do |t|
@@ -90,14 +89,14 @@ ActiveRecord::Schema.define(version: 2022_06_17_190447) do
     t.string "related_change_type"
     t.integer "related_change_id"
     t.string "description"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
   end
 
   create_table "merit_score_points", force: :cascade do |t|
     t.integer "score_id"
     t.bigint "num_points", default: 0
     t.string "log"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
     t.index ["score_id"], name: "index_merit_score_points_on_score_id"
   end
 
@@ -110,8 +109,8 @@ ActiveRecord::Schema.define(version: 2022_06_17_190447) do
   create_table "players", force: :cascade do |t|
     t.string "username"
     t.string "avatar_url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "sash_id"
     t.integer "level", default: 0
     t.boolean "vip"
@@ -124,23 +123,23 @@ ActiveRecord::Schema.define(version: 2022_06_17_190447) do
 
   create_table "rounds", force: :cascade do |t|
     t.integer "match_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "team_a_score", default: 0
     t.integer "team_b_score", default: 0
     t.index ["match_id"], name: "index_rounds_on_match_id"
   end
 
   create_table "sashes", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "true_skill_ratings", force: :cascade do |t|
     t.float "mean"
     t.float "deviation"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "player_id"
     t.integer "match_id"
     t.index ["match_id"], name: "index_true_skill_ratings_on_match_id"
