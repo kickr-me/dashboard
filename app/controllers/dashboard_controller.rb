518e4196dfaa3623251122bba03ee67f66f49dea
Flipez <code@brauser.io>
2022-11-30 15:37:53 +0100

    Include only players with >6 matches in top10
    Signed-off-by: Flipez <code@brauser.io>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
     def index
         @last_match = Match.last
-        @skill_players = Player.all.select{|p| p.matches.size > 2}.sort_by{|p| p.skill.mean}.last(10)
+        @skill_players = Player.all.select{|p| p.matches.size > 6}.sort_by{|p| p.skill.mean}.last(10)
     end
 end
